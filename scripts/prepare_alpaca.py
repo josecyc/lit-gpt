f78e0e372a5285de8bf93380f4ec7c2ce1f6af4d
Jose Cruz y Celis <jcruzycelis@gmail.com>
2023-06-30 20:40:22 -0600

    wer

--- scripts/prepare_alpaca.py
+++ scripts/prepare_alpaca.py
@@ -16,47 +16,61 @@ from lit_gpt.tokenizer import Tokenizer
 
 DATA_FILE_URL = "https://raw.githubusercontent.com/tloen/alpaca-lora/main/alpaca_data_cleaned_archive.json"
 DATA_FILE_NAME = "alpaca_data_cleaned_archive.json"
-DESTINATION_PATH = Path("data/alpaca")
+DESTINATION_PATH = Path("../deq/adstop/adstop-back/")
 CHECKPOINT_DIR = Path("checkpoints/stabilityai/stablelm-base-alpha-3b")
-TEST_SPLIT_SIZE = 2000
+TEST_SPLIT_SIZE = 300
 IGNORE_INDEX = -1
 MASK_INPUTS = False  # as in alpaca-lora
 SEED = 42
 
 
+import os
+import glob
+import os
+import glob
+from typing import List, Dict
+
+def load_codebase(root_dir: str) -> Dict[str, str]:
+    """Load the codebase from a root directory."""
+    data = {}
+    for dirpath, dirs, files in os.walk(root_dir):
+        for filename in glob.glob(os.path.join(dirpath, '*.*')):  # or whatever your file type is
+            with open(filename, 'r', encoding='utf-8', errors='ignore') as file:
+            # with open(filepath, 'r', encoding='your_encoding') as file:
+                data[filename] = file.read()
+                print("filename", filename)
+    return data
+
 def prepare(
+    root_dir: str,
     destination_path: Path = DESTINATION_PATH,
     checkpoint_dir: Path = CHECKPOINT_DIR,
     test_split_size: int = TEST_SPLIT_SIZE,
     seed: int = SEED,
     mask_inputs: bool = MASK_INPUTS,
-    data_file_name: str = DATA_FILE_NAME,
-    data_file_url: str = DATA_FILE_URL,
     ignore_index: int = IGNORE_INDEX,
 ) -> None:
-    """Prepare the Alpaca dataset for instruction tuning.
+    """Prepare the codebase for fine-tuning.
 
     The output is a training and test dataset saved as `train.pt` and `test.pt`,
-    which stores the preprocessed and tokenized prompts and labels.
+    which stores the preprocessed and tokenized files' content.
     """
     with open(checkpoint_dir / "lit_config.json", "r") as file:
         config = json.load(file)
         max_seq_length = config["block_size"]
 
     destination_path.mkdir(parents=True, exist_ok=True)
-    data_file_path = destination_path / data_file_name
-    print("Loading data file...")
-    download_if_missing(data_file_path, data_file_url)
-    with open(data_file_path, "r", encoding="utf-8") as file:
-        data = json.load(file)
+    print("Loading codebase...")
+    data = load_codebase(root_dir)
+    data_items = list(data.items())
 
     print("Loading tokenizer...")
     tokenizer = Tokenizer(checkpoint_dir)
 
     # Partition the dataset into train and test
-    train_split_size = len(data) - test_split_size
+    train_split_size = len(data_items) - test_split_size
     train_set, test_set = random_split(
-        data, lengths=(train_split_size, test_split_size), generator=torch.Generator().manual_seed(seed)
+        data_items, lengths=(train_split_size, test_split_size), generator=torch.Generator().manual_seed(seed)
     )
     train_set, test_set = list(train_set), list(test_set)
 
@@ -66,7 +80,8 @@ def prepare(
     print("Processing train split ...")
     train_set = [
         prepare_sample(
-            example=sample,
+            sample[0],
+            content=sample[1],
             tokenizer=tokenizer,
             max_length=max_seq_length,
             mask_inputs=mask_inputs,
@@ -79,7 +94,8 @@ def prepare(
     print("Processing test split ...")
     test_set = [
         prepare_sample(
-            example=sample,
+            sample[0],
+            content=sample[1],
             tokenizer=tokenizer,
             max_length=max_seq_length,
             mask_inputs=mask_inputs,
@@ -87,7 +103,8 @@ def prepare(
         )
         for sample in tqdm(test_set)
     ]
-    torch.save(test_set, destination_path / "test.pt")
+    torch.save(train_set, destination_path / "test.pt")
+
 
 
 def download_if_missing(file_path: Path, file_url: str):
@@ -97,31 +114,22 @@ def download_if_missing(file_path: Path, file_url: str):
     with open(file_path, "w", encoding="utf-8") as f:
         f.write(requests.get(file_url).text)
 
-
 def prepare_sample(
-    example: dict,
+    filepath: str,
+    content: str,
     tokenizer: Tokenizer,
     max_length: int,
     mask_inputs: bool = MASK_INPUTS,
     ignore_index: int = IGNORE_INDEX,
 ):
-    """Processes a single sample.
+    example = {"output": content}
 
-    Each sample in the dataset consists of:
-    - instruction: A string describing the task
-    - input: A string holding a special input value for the instruction.
-        This only applies to some samples, and in others this is empty.
-    - output: The response string
+    with open(filepath, 'r', encoding='ISO-8859-1') as file:
+        example = {"output": file.read()}
 
-    This function processes this data to produce a prompt text and a label for
-    supervised training. The prompt text is formed as a single message including both
-    the instruction and the input. The label/target is the same message but with the
-    response attached.
 
-    Finally, both the prompt and the label get tokenized. If desired, all tokens
-    in the label that correspond to the original input prompt get masked out (default).
-    """
-    full_prompt = generate_prompt(example)
+    # Assume full_prompt is the same as output in this case
+    full_prompt = example["output"]
     full_prompt_and_response = full_prompt + example["output"]
     encoded_full_prompt = tokenizer.encode(full_prompt, max_length=max_length)
     encoded_full_prompt_and_response = tokenizer.encode(full_prompt_and_response, eos=True, max_length=max_length)
@@ -139,24 +147,165 @@ def prepare_sample(
     }
 
 
-def generate_prompt(example):
-    """Generates a standardized message to prompt the model with an instruction, optional input and a
-    'response' field."""
 
-    if example["input"]:
-        return (
-            "Below is an instruction that describes a task, paired with an input that provides further context. "
-            "Write a response that appropriately completes the request.\n\n"
-            f"### Instruction:\n{example['instruction']}\n\n### Input:\n{example['input']}\n\n### Response:"
-        )
-    return (
-        "Below is an instruction that describes a task. "
-        "Write a response that appropriately completes the request.\n\n"
-        f"### Instruction:\n{example['instruction']}\n\n### Response:"
-    )
+def generate_prompt(example: dict):
+    prompt = example.get("output", "")  # replace "output" with the actual key in your data if needed
+    return prompt
+
 
 
 if __name__ == "__main__":
     from jsonargparse import CLI
 
     CLI(prepare)
+
+# def load_codebase(root_dir):
+#     """Load the codebase from a root directory."""
+#     data = {}
+#     for dirpath, dirs, files in os.walk(root_dir):
+#         for filename in glob.glob(os.path.join(dirpath, '*.py')):  # or whatever your file type is
+#             with open(filename, 'r', encoding='utf-8', errors='ignore') as file:
+#                 data[filename] = file.read()
+#     return data
+
+# def prepare(
+#     destination_path: Path = DESTINATION_PATH,
+#     checkpoint_dir: Path = CHECKPOINT_DIR,
+#     test_split_size: int = TEST_SPLIT_SIZE,
+#     seed: int = SEED,
+#     mask_inputs: bool = MASK_INPUTS,
+#     root_dir: str = "/path/to/your/codebase",  # path to your local codebase
+#     ignore_index: int = IGNORE_INDEX,
+# ) -> None:
+#     """Prepare the Alpaca dataset for instruction tuning.
+
+#     The output is a training and test dataset saved as `train.pt` and `test.pt`,
+#     which stores the preprocessed and tokenized prompts and labels.
+#     """
+#     with open(checkpoint_dir / "lit_config.json", "r") as file:
+#         config = json.load(file)
+#         max_seq_length = config["block_size"]
+
+#     destination_path.mkdir(parents=True, exist_ok=True)
+#     print("Loading data file...")
+#     data = load_codebase(root_dir)
+
+# def prepare(
+#     destination_path: Path = DESTINATION_PATH,
+#     checkpoint_dir: Path = CHECKPOINT_DIR,
+#     test_split_size: int = TEST_SPLIT_SIZE,
+#     seed: int = SEED,
+#     mask_inputs: bool = MASK_INPUTS,
+#     data_file_name: str = DATA_FILE_NAME,
+#     data_file_url: str = DATA_FILE_URL,
+#     ignore_index: int = IGNORE_INDEX,
+# ) -> None:
+#     """Prepare the Alpaca dataset for instruction tuning.
+
+#     The output is a training and test dataset saved as `train.pt` and `test.pt`,
+#     which stores the preprocessed and tokenized prompts and labels.
+#     """
+#     with open(checkpoint_dir / "lit_config.json", "r") as file:
+#         config = json.load(file)
+#         max_seq_length = config["block_size"]
+
+#     destination_path.mkdir(parents=True, exist_ok=True)
+#     data_file_path = destination_path / data_file_name
+#     print("Loading data file...")
+#     download_if_missing(data_file_path, data_file_url)
+#     with open(data_file_path, "r", encoding="utf-8") as file:
+#         data = json.load(file)
+
+#     print("Loading tokenizer...")
+#     tokenizer = Tokenizer(checkpoint_dir)
+
+#     # Partition the dataset into train and test
+#     train_split_size = len(data) - test_split_size
+#     train_set, test_set = random_split(
+#         data, lengths=(train_split_size, test_split_size), generator=torch.Generator().manual_seed(seed)
+#     )
+#     train_set, test_set = list(train_set), list(test_set)
+
+#     print(f"train has {len(train_set):,} samples")
+#     print(f"test has {len(test_set):,} samples")
+
+#     print("Processing train split ...")
+#     train_set = [
+#         prepare_sample(
+#             example=sample,
+#             tokenizer=tokenizer,
+#             max_length=max_seq_length,
+#             mask_inputs=mask_inputs,
+#             ignore_index=ignore_index,
+#         )
+#         for sample in tqdm(train_set)
+#     ]
+#     torch.save(train_set, destination_path / "train.pt")
+
+#     print("Processing test split ...")
+#     test_set = [
+#         prepare_sample(
+#             example=sample,
+#             tokenizer=tokenizer,
+#             max_length=max_seq_length,
+#             mask_inputs=mask_inputs,
+#             ignore_index=ignore_index,
+#         )
+#         for sample in tqdm(test_set)
+#     ]
+#     torch.save(test_set, destination_path / "test.pt")
+# def prepare_sample(
+#     example: dict,
+#     tokenizer: Tokenizer,
+#     max_length: int,
+#     mask_inputs: bool = MASK_INPUTS,
+#     ignore_index: int = IGNORE_INDEX,
+# ):
+#     """Processes a single sample.
+
+#     Each sample in the dataset consists of:
+#     - instruction: A string describing the task
+#     - input: A string holding a special input value for the instruction.
+#         This only applies to some samples, and in others this is empty.
+#     - output: The response string
+
+#     This function processes this data to produce a prompt text and a label for
+#     supervised training. The prompt text is formed as a single message including both
+#     the instruction and the input. The label/target is the same message but with the
+#     response attached.
+
+#     Finally, both the prompt and the label get tokenized. If desired, all tokens
+#     in the label that correspond to the original input prompt get masked out (default).
+#     """
+#     full_prompt = generate_prompt(example)
+#     full_prompt_and_response = full_prompt + example["output"]
+#     encoded_full_prompt = tokenizer.encode(full_prompt, max_length=max_length)
+#     encoded_full_prompt_and_response = tokenizer.encode(full_prompt_and_response, eos=True, max_length=max_length)
+
+#     # The labels are the full prompt with response, but with the prompt masked out
+#     labels = encoded_full_prompt_and_response.clone()
+#     if mask_inputs:
+#         labels[: len(encoded_full_prompt)] = ignore_index
+
+#     return {
+#         **example,
+#         "input_ids": encoded_full_prompt_and_response,
+#         "input_ids_no_response": encoded_full_prompt,
+#         "labels": labels,
+#     }
+
+# def generate_prompt(example):
+#     """Generates a standardized message to prompt the model with an instruction, optional input and a
+#     'response' field."""
+
+#     if example["input"]:
+#         return (
+#             "Below is an instruction that describes a task, paired with an input that provides further context. "
+#             "Write a response that appropriately completes the request.\n\n"
+#             f"### Instruction:\n{example['instruction']}\n\n### Input:\n{example['input']}\n\n### Response:"
+#         )
+#     return (
+#         "Below is an instruction that describes a task. "
+#         "Write a response that appropriately completes the request.\n\n"
+#         f"### Instruction:\n{example['instruction']}\n\n### Response:"
+#     )
